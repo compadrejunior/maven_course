ddfd14a77234c2a1ecefad76622eead5fb64d067
José Compadre Junior <jose.compadre@oatsolutions.com.br>
2021-12-15 13:32:45 -0300

    Adding .gitignore files

---
+++ hello-world/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * File: HelloWorld
+ * Project: hello-world
+ * File Created: 30/11/2021 17:11
+ * Author: compadrejunior
+ * Copyright (c) 2021 OAT Solutions, OAT Solutions. All rights reserved.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
